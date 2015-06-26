3fcf3e0c8e6a2214fee769d128bdbd74816ab925
Mike Oksenenko <mmx.mmx@gmail.com>
2015-06-26 03:17:34 +0300

    rm import

--- src/main/java/com/blogspot/mikelaud/je/OpenType.java
+++ src/main/java/com/blogspot/mikelaud/je/OpenType.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
