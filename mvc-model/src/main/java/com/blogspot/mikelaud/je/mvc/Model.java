3d40fbc9522d1e2333265a1f33bfda228a0fef34
Mike Oksenenko <mmx.mmx@gmail.com>
2015-12-27 04:39:51 +0300

    sync

--- mvc-model/src/main/java/com/blogspot/mikelaud/je/mvc/Model.java
+++ mvc-model/src/main/java/com/blogspot/mikelaud/je/mvc/Model.java
@@ -1,5 +1,23 @@
 package com.blogspot.mikelaud.je.mvc;
 
+import java.util.Collection;
+
+import com.blogspot.mikelaud.je.mvc.domain.Type;
+
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+
 public interface Model {
 
+	void setTypesSource(String aTypesSource);
+	String getTypesSource();
+	StringProperty takeTypesSource();
+	//
+	void setTypes(Collection<Type> aTypes);
+	ObservableList<Type> getTypes();
+	FilteredList<Type> getTypesFiltered();
+	SortedList<Type> getTypesSorted();
+	
 }
