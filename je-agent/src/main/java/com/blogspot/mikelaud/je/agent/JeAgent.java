e3fa404e4e3f49d9eca333907228f9fed6d84dfa
Mike Oksenenko <mmx.mmx@gmail.com>
2016-01-08 02:22:15 +0300

    add agent manifest

---
+++ je-agent/src/main/java/com/blogspot/mikelaud/je/agent/JeAgent.java
@@ -0,0 +1,11 @@
+package com.blogspot.mikelaud.je.agent;
+
+import java.lang.instrument.Instrumentation;
+
+public class JeAgent {
+
+	public static void agentmain(String agentArgs, Instrumentation inst) {
+		// void
+	}
+	
+}
