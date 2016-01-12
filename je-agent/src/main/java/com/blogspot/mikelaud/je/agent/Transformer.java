4c6eef00ec00c7778fa65586c0a6bcc27ac4d7f6
Mike Oksenenko <mmx.mmx@gmail.com>
2016-01-12 10:01:39 +0300

    load all classes

--- je-agent/src/main/java/com/blogspot/mikelaud/je/agent/Transformer.java
+++ je-agent/src/main/java/com/blogspot/mikelaud/je/agent/Transformer.java
@@ -3,24 +3,16 @@ package com.blogspot.mikelaud.je.agent;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
-import java.util.List;
 
 public class Transformer implements ClassFileTransformer {
 
-	private final List<byte[]> BYTECODES;
-	
 	@Override
 	public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
 			ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
 		if (className.startsWith("com/blogspot/mikelaud")) {
 			System.out.println("[agent] transform class: " + className);
 		}
-		BYTECODES.add(classfileBuffer);
 		return null;
-	}
-
-	public Transformer(List<byte[]> aBytecodes) {
-		BYTECODES = aBytecodes;
 	}
 	
 }
--- je-agent/src/main/java/com/blogspot/mikelaud/je/agent/beans/Types.java
+++ je-agent/src/main/java/com/blogspot/mikelaud/je/agent/beans/Types.java
@@ -1,9 +1,11 @@
 package com.blogspot.mikelaud.je.agent.beans;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.instrument.Instrumentation;
-import java.lang.instrument.UnmodifiableClassException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import com.blogspot.mikelaud.je.agent.Transformer;
 
@@ -14,8 +16,8 @@ public class Types implements TypesMXBean {
 
 	public Types(Instrumentation aInstrumentation) {
 		INSTRUMENTATION = aInstrumentation;
-		BYTECODES = new CopyOnWriteArrayList<>();
-		INSTRUMENTATION.addTransformer(new Transformer(BYTECODES));
+		BYTECODES = new ArrayList<>();
+		INSTRUMENTATION.addTransformer(new Transformer());
 	}
 
 	@Override
@@ -23,21 +25,42 @@ public class Types implements TypesMXBean {
 		System.out.println("[agent] echo.");
 	}
 
+	private byte[] readBytes(InputStream aInputStream) {
+		try {
+			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+			int nRead;
+			byte[] data = new byte[16384];
+			while ((nRead = aInputStream.read(data, 0, data.length)) != -1) {
+				buffer.write(data, 0, nRead);
+			}
+			buffer.flush();
+			return buffer.toByteArray();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			return new byte[0];
+		}
+	}
+	
 	@Override
 	public List<byte[]> getBytecodes() {
 		for (Class<?> clazz : INSTRUMENTATION.getAllLoadedClasses()) {
 			if (clazz.getName().contains("$Lambda$")) continue;
 			if (clazz.getName().startsWith("java.lang.")) continue;
 			if (clazz.getName().startsWith("[")) continue;
-			if (clazz.getName().startsWith("com.blogspot.mikelaud")) {
-				System.out.println("[agent] retransform self class: " + clazz.getName());
+			final String name = clazz.getName().replaceAll("\\.", "/") + ".class";
+			ClassLoader classLoader = clazz.getClassLoader();
+			if (null == classLoader) {
+				System.out.println("[agent] skip (no class loader): " + name);
+				continue;
 			}
-			try {
-				INSTRUMENTATION.retransformClasses(clazz);
+			InputStream stream = classLoader.getResourceAsStream(name);
+			if (null == stream) {
+				System.out.println("[agent] skip (no class stream): " + name);
+				continue;
 			}
-			catch (UnmodifiableClassException e) {
-				System.err.println("[agent] skip class: " + clazz.getName() + "; " + e);
-			}
+			byte[] bytecode = readBytes(stream);
+			BYTECODES.add(bytecode);
 		}
 		return BYTECODES;
 	}
