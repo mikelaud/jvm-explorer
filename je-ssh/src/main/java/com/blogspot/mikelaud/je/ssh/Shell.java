3b534071f51d2b14ce023ae75f5a21a61d7a061b
Mike Oksenenko <mmx.mmx@gmail.com>
2016-01-26 22:47:03 +0300

    format

--- je-ssh/src/main/java/com/blogspot/mikelaud/je/ssh/Shell.java
+++ je-ssh/src/main/java/com/blogspot/mikelaud/je/ssh/Shell.java
@@ -1,33 +1,35 @@
 package com.blogspot.mikelaud.je.ssh;
 
-
 import com.jcraft.jsch.*;
 import javax.swing.*;
 
 public class Shell {
 
-  public static void main(String[] arg) {
-    try {
-      JSch jsch = new JSch();
-      String connstr = null;
-      if (arg.length > 0) {
-        connstr = arg[0];
-      } else {
-        connstr = JOptionPane.showInputDialog("Enter username:password@hostname", System.getProperty("user.name")+":<pass>"+"@localhost");
-      }
-      String user = connstr.substring(0, connstr.indexOf(':'));
-      String pass = connstr.substring(connstr.indexOf(':') + 1, connstr.indexOf('@'));
-      String host = connstr.substring(connstr.indexOf('@') + 1);
-      Session session = jsch.getSession(user, host, 22);
-      session.setPassword(pass);
-      session.setConfig("StrictHostKeyChecking", "no");
-      session.connect();
-      Channel channel = session.openChannel("shell");
-      channel.setInputStream(System.in);
-      channel.setOutputStream(System.out);
-      channel.connect();
-    } catch (Exception e) {
-      System.out.println(e);
-    }
-  }
+	public static void main(String[] arg) {
+		try {
+			JSch jsch = new JSch();
+			String connstr = null;
+			if (arg.length > 0) {
+				connstr = arg[0];
+			}
+			else {
+				connstr = JOptionPane.showInputDialog("Enter username:password@hostname",
+						System.getProperty("user.name") + ":<pass>" + "@localhost");
+			}
+			String user = connstr.substring(0, connstr.indexOf(':'));
+			String pass = connstr.substring(connstr.indexOf(':') + 1, connstr.indexOf('@'));
+			String host = connstr.substring(connstr.indexOf('@') + 1);
+			Session session = jsch.getSession(user, host, 22);
+			session.setPassword(pass);
+			session.setConfig("StrictHostKeyChecking", "no");
+			session.connect();
+			Channel channel = session.openChannel("shell");
+			channel.setInputStream(System.in);
+			channel.setOutputStream(System.out);
+			channel.connect();
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
