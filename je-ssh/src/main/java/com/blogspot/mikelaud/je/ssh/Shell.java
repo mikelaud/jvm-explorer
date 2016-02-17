d442881f357efff335beba3943ec5f957e5bc288
Mike Oksenenko <mmx.mmx@gmail.com>
2016-02-17 23:51:30 +0000

    add remote exec

--- je-ssh/src/main/java/com/blogspot/mikelaud/je/ssh/Shell.java
+++ je-ssh/src/main/java/com/blogspot/mikelaud/je/ssh/Shell.java
@@ -1,39 +1,46 @@
 package com.blogspot.mikelaud.je.ssh;
 
-import com.jcraft.jsch.*;
-import javax.swing.*;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 
 public class Shell {
+
+	public static int exec(Session aSession, String aCommand) {
+		int rcode = 0;
+		try {
+			ChannelExec channel = (ChannelExec)aSession.openChannel("exec");
+			channel.setCommand(aCommand);
+			channel.setInputStream(null);
+			channel.setOutputStream(System.out, true);
+			channel.setErrStream(System.out, true);
+			channel.connect();
+			while (!channel.isEOF()) {
+				try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
+			}
+			rcode = channel.getExitStatus();
+			channel.disconnect();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			rcode = 1;
+		}
+		return rcode;
+	}
 
 	public static void main(String[] arg) {
 		try {
 			JSch jsch = new JSch();
-			String connstr = null;
-			if (arg.length > 0) {
-				connstr = arg[0];
-			}
-			else {
-				connstr = JOptionPane.showInputDialog(
-					"Enter username:password@hostname",
-					System.getProperty("user.name") + ":<pass>" + "@localhost"
-				);
-			}
-			String user = connstr.substring(0, connstr.indexOf(':'));
-			String pass = connstr.substring(connstr.indexOf(':') + 1, connstr.indexOf('@'));
-			String host = connstr.substring(connstr.indexOf('@') + 1);
-			//
-			Session session = jsch.getSession(user, host, 22);
-			session.setPassword(pass);
+			Session session = jsch.getSession("root", "192.168.10.101", 22);
+			session.setPassword("xxx");
 			session.setConfig("StrictHostKeyChecking", "no");
 			session.connect();
-			//
-			Channel channel = session.openChannel("shell");
-			channel.setInputStream(System.in);
-			channel.setOutputStream(System.out);
-			channel.connect();
+			System.out.println("exit status: "+ exec(session, "ls -l"));
+			System.out.println("exit status: "+ exec(session, "ls"));
+		    session.disconnect();
 		}
 		catch (Exception e) {
-			System.out.println(e);
+			e.printStackTrace();
 		}
 	}
 }
