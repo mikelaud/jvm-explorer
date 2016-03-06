9601646de4cb10f3aaef647148b732fda51b7065
Mike Oksenenko <mmx.mmx@gmail.com>
2016-03-06 14:02:50 +0300

    sync

--- jvm-explorer-client/jvm-explorer-client-ui/ui-resources/src/main/java/com/blogspot/mikelaud/je/ui/resources/UiResourcesImpl.java
+++ jvm-explorer-client/jvm-explorer-client-ui/ui-resources/src/main/java/com/blogspot/mikelaud/je/ui/resources/UiResourcesImpl.java
@@ -1,10 +1,12 @@
 package com.blogspot.mikelaud.je.ui.resources;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.blogspot.mikelaud.je.mvc.MvcController;
+import com.blogspot.mikelaud.je.ssh.common.UnixPath;
 import com.google.inject.Inject;
 
 import javafx.scene.Scene;
@@ -14,12 +16,14 @@ import javafx.scene.image.WritableImage;
 public class UiResourcesImpl implements UiResources {
 
 	private final MvcController CONTROLLER;
+	private final Path IMAGES_LOCATION;
 	private final Map<Path, Image> IMAGES;
 	private final Image EMPTY_IMAGE;
 
 	@Inject
 	private UiResourcesImpl(MvcController aController) {
 		CONTROLLER = aController;
+		IMAGES_LOCATION = Paths.get("META-INF/resources/images");
 		IMAGES = new HashMap<>();
 		EMPTY_IMAGE = new WritableImage(1, 1);
 	}
@@ -28,7 +32,8 @@ public class UiResourcesImpl implements UiResources {
 		System.out.println("Load image: ".concat(aPath.toString()));
 		Image image = EMPTY_IMAGE;
 		try {
-			image = new Image(aPath.toString());
+			UnixPath jarPath = new UnixPath(IMAGES_LOCATION.resolve(aPath));
+			image = new Image(jarPath.toString());
 			IMAGES.put(aPath, image);
 		}
 		catch (IllegalArgumentException e) {
@@ -37,7 +42,7 @@ public class UiResourcesImpl implements UiResources {
 		}
 		return image;
 	}
-	
+
 	@Override
 	public final Image getImage(Path aPath) {
 		Image image = IMAGES.get(aPath);
@@ -50,7 +55,7 @@ public class UiResourcesImpl implements UiResources {
 	@Override
 	public final void loadCss() {
 		Scene scene = CONTROLLER.getModel().getStage().getScene();
-		scene.getStylesheets().add("je.css");
+		scene.getStylesheets().add("META-INF/resources/je.css");
 	}
 
 }
